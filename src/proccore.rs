a2281d62db5b4180c89ce45e6292631b32baef25
RepeatedRoot <choeffe@gmail.com>
2021-09-23 21:25:39 +0930

    add support for no equivalent arm instruction

--- src/proccore.rs
+++ src/proccore.rs
@@ -184,34 +184,6 @@ impl Core {
         }
     }
 
-    pub fn addr_reg(&self) {
-
-    }
-
-    pub fn addr_inc(&self) {
-
-    }
-
-    pub fn reg_bank(&self) {
-
-    }
-
-    pub fn mult(&self) {
-
-    }
-
-    pub fn barrel_shift(&self) {
-
-    }
-
-    pub fn write_reg(&self) {
-
-    }
-
-    pub fn inst_pipe(&self) {
-
-    }
-
     fn read_flag(&self, flag: u8) -> u32 {
         match flag {
             0 ..= 3 => (&self.reg.cpsr & (1<<(31-flag))) >> 31-flag,
@@ -250,6 +222,7 @@ impl Core {
     }
 }
 
+/* The following two decoding functions are not in order of instruction formats, but are ordered such that they work */
 #[allow(dead_code)]
 pub fn decode_arm(inst: u32) -> ArmInstType {
     if bitpat!( _ _ _ _ 0 0 0 1 0 0 1 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 1 _ _ _ _ )(inst) {ArmInstType::BranchAndExchange}                       else
@@ -270,7 +243,6 @@ pub fn decode_arm(inst: u32) -> ArmInstType {
     {ArmInstType::Undefined}
 }
 
-/* This is not the order instruction appear in the technical reference, but are ordered such that decoding works */
 #[allow(dead_code)]
 pub fn decode_thumb(inst: u16) -> ThumbInstType {
     if bitpat!( 1 1 0 1 1 1 1 1 _ _ _ _ _ _ _ _ )(inst) {ThumbInstType::SoftwareInterrupt} else
@@ -296,21 +268,21 @@ pub fn decode_thumb(inst: u16) -> ThumbInstType {
 }
 
 #[allow(dead_code)]
-pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
+pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> Option<u32> {
     match insttype {
         ThumbInstType::SoftwareInterrupt => {
-            (inst & 0x00FF) as u32 | 0xEF000000
+            Some((inst & 0x00FF) as u32 | 0xEF000000)
         },
         ThumbInstType::AddOffsetToStackPointer => {
             let s: u32 = ((inst & 0x80)>>7) as u32;
             let sword7 = (inst & 0x7F) as u32;
-            0b11100000000011011101000000000000 | sword7 | (1 << 23-s)
+            Some(0b11100000000011011101000000000000 | sword7 | (1 << 23-s))
         },
         ThumbInstType::PushPopRegisters => {
             let l: u32  = ((inst & 0x800) >> 11) as u32;
             let r: u32  = ((inst & 0x100) >> 8) as u32;
             let rlist: u32 = (inst & 0xFF) as u32;
-            0b11101000001011010000000000000000 | rlist | (((r&!l)&0b1)<<14) | ((r&l)<<15) | (l << 20) | (l << 23) | ((!l&0b1)<<24)
+            Some(0b11101000001011010000000000000000 | rlist | (((r&!l)&0b1)<<14) | ((r&l)<<15) | (l << 20) | (l << 23) | ((!l&0b1)<<24))
         },
         ThumbInstType::LoadStoreWithRegisterOffset => {
             let l: u32  = ((inst & 0x800) >> 11) as u32;
@@ -318,7 +290,7 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let ro: u32 = ((inst & 0x1C0) >> 6) as u32;
             let rb: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
-            0b11100111100000000000000000000000 | ro | (rd << 12) | (rb << 16) | (b << 22) | (l << 20)
+            Some(0b11100111100000000000000000000000 | ro | (rd << 12) | (rb << 16) | (b << 22) | (l << 20))
         },
         ThumbInstType::LoadStoreSignExtendedByteHalfword => {
             let h: u32  = ((inst & 0x800) >> 11) as u32;
@@ -326,7 +298,7 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let ro: u32 = ((inst & 0x1C0) >> 6) as u32;
             let rb: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
-            0b11100001100000000000000010010000 | ro | (h << 5) | (s << 6) | (rd << 12) | (rb << 16) | ((s|h) << 20)
+            Some(0b11100001100000000000000010010000 | ro | (h << 5) | (s << 6) | (rd << 12) | (rb << 16) | ((s|h) << 20))
         },
         ThumbInstType::ALUOperation => {
             let mut op = ((inst & 0x3C0) >> 6) as u32;
@@ -335,9 +307,9 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let shift = if op==0x7 {0b0111} else {0b0};
             if op==0xD {
                 /* Multiply instruction */
-                0b11100000000100000000000010010000 | rs | (rd << 8) | (rd << 16)
+                Some(0b11100000000100000000000010010000 | rs | (rd << 8) | (rd << 16))
             } else if op==0x9 {
-                0b11100010011100000000000000000000 | (rd << 12 ) | (rs << 16)
+                Some(0b11100010011100000000000000000000 | (rd << 12 ) | (rs << 16))
             } else {
                 /* Data processing and PSR transfer */
                 op = match op {
@@ -346,7 +318,7 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
                     0x9         => 0x3,
                     _           => op
                 };
-                0b11100000000100000000000000000000 | rs | (shift << 4) | (rd << 12) | (rd << 16) | (op << 21)
+                Some(0b11100000000100000000000000000000 | rs | (shift << 4) | (rd << 12) | (rd << 16) | (op << 21))
             }
         },
         ThumbInstType::HiRegisterOperationsBranchExchange => {
@@ -365,9 +337,9 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             rshs = rshs | (h2 << 3);
             rdhd = rdhd | (h1 << 3);
             if op == 0 {
-                0b11100001001011111111111100010000 | rshs
+                Some(0b11100001001011111111111100010000 | rshs)
             } else {
-                0b11100000000000000000000000000000 | rshs | (rdhd << 12) | (rdhd << 16) | if op==10 {0b1<<20} else {0x0} | (op << 21)
+                Some(0b11100000000000000000000000000000 | rshs | (rdhd << 12) | (rdhd << 16) | if op==10 {0b1<<20} else {0x0} | (op << 21))
             }
         },
         ThumbInstType::AddSubtract => {
@@ -377,16 +349,16 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let rs: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
             let top: u32 = (op << 1) | ((!op&0b1) << 2);
-            0b11100000000100000000000000000000 | rn | (rd << 12) | (rs << 16) | (top<<21) | (i << 25)
+            Some(0b11100000000100000000000000000000 | rn | (rd << 12) | (rs << 16) | (top<<21) | (i << 25))
         },
         ThumbInstType::UnconditionalBranch => {
             let off11: u32 = (inst &0x7FF) as u32;
-            0b11101010000000000000000000000000 | (off11 >> 1)
+            Some(0b11101010000000000000000000000000 | (off11 >> 1))
         },
         ThumbInstType::PCRelativeLoad => {
             let rd: u32 = ((inst & 0x700) >> 8) as u32;
             let word8: u32 = (inst & 0xFF) as u32;
-            0b11100101100111110000000000000000 | (word8 << 2) | (rd << 12)
+            Some(0b11100101100111110000000000000000 | (word8 << 2) | (rd << 12))
         },
         ThumbInstType::LoadStoreHalfword => {
             let l: u32 = ((inst & 0x800) >> 11) as u32;
@@ -395,42 +367,41 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let offlo: u32 = offset5 & 0xF;
             let rb: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
-            0b11100001110000000000000010110000 | (rb << 16) | (rd << 12 ) | (l << 20) | (offhi << 8) | offlo
+            Some(0b11100001110000000000000010110000 | (rb << 16) | (rd << 12 ) | (l << 20) | (offhi << 8) | offlo)
         },
         ThumbInstType::SPRelativeLoadStore => {
             let l: u32 = ((inst & 0x800) >> 11) as u32;
             let rd: u32 = ((inst & 0x700) >> 8) as u32;
             let word8: u32 = (inst & 0xFF) as u32;
-            0b11100101100011010000000000000000 | word8 | (rd << 12) | (l << 20)
+            Some(0b11100101100011010000000000000000 | word8 | (rd << 12) | (l << 20))
         },
         ThumbInstType::LoadAddress => {
             let sp: u32 = ((inst & 0x800) >> 11) as u32;
             let rd: u32 = ((inst & 0x700) >> 8) as u32;
             let word8: u32 = (inst & 0xFF) as u32;
-            0b11100010100011010000001000000000 | word8 | (rd << 12) | ((!sp&0b1) << 17)
+            Some(0b11100010100011010000001000000000 | word8 | (rd << 12) | ((!sp&0b1) << 17))
         },
         ThumbInstType::MultipleLoadStore => {
             let l: u32 = ((inst & 0x800) >> 11) as u32;
             let rb: u32 = ((inst & 0x700) >> 8) as u32;
             let rlist: u32 = (inst & 0xFF) as u32;
-            0b11101000101000000000000000000000 | rlist | (rb << 16) | (l << 20)
+            Some(0b11101000101000000000000000000000 | rlist | (rb << 16) | (l << 20))
         },
         ThumbInstType::ConditionalBranch => {
             let cond: u32 = ((inst & 0xF00) >> 8) as u32;
             let soff: u32 = (inst & 0xFF) as u32;
-            0b00001010000000000000000000000000 | soff | (cond << 28)
+            Some(0b00001010000000000000000000000000 | soff | (cond << 28))
         },
         ThumbInstType::LongBranchWithLink => {
             /* No equivalent */
-            0
+            None
         },
         ThumbInstType::MoveShiftedRegister => {
             let op: u32 = ((inst & 0x1800) >> 11) as u32;
             let off5: u32 = ((inst & 0x7C0) >> 6) as u32;
             let rs: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
-            println!("op {} off5 {} rs {} rd {}", op, off5, rs, rd);
-            0b11100001101100000000000000000000 | rs | (op << 5) | (off5 << 7) | (rd << 12)
+            Some(0b11100001101100000000000000000000 | rs | (op << 5) | (off5 << 7) | (rd << 12))
         },
         ThumbInstType::MoveCompareAddSubtractImmediate => {
             let mut op: u32 = ((inst & 0x1800) >> 11) as u32;
@@ -444,7 +415,7 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
                 3   => 0b0010,
                 _   => panic!("Format 3 opcode {} does not exist", op)
             };
-            0b11100010000100000000000000000000 | off8 | (rd << 12) | (rd << 16) | (op << 21)
+            Some(0b11100010000100000000000000000000 | off8 | (rd << 12) | (rd << 16) | (op << 21))
         },
         ThumbInstType::LoadStoreWithImmediateOffset => {
             let b: u32 = ((inst & 0x1000) >> 12) as u32;
@@ -452,11 +423,10 @@ pub fn translate_thumb(inst: u16, insttype: ThumbInstType) -> u32 {
             let off5: u32 = ((inst & 0x7C0) >> 6) as u32;
             let rb: u32 = ((inst & 0x38) >> 3) as u32;
             let rd: u32 = (inst & 0x7) as u32;
-            0b11100101100000000000000000000000 | off5 | (rd << 12) | (rb << 16) | (l << 20) | (b << 22)
+            Some(0b11100101100000000000000000000000 | off5 | (rd << 12) | (rb << 16) | (l << 20) | (b << 22))
         },
         ThumbInstType::Undefined => {
-            0b11100110000000000000000000010000
+            Some(0b11100110000000000000000000010000)
         }
-        _   =>  panic!("Thumb instruction is not implemented!")
     }
 }
--- tests/thumb_inst_conversion.rs
+++ tests/thumb_inst_conversion.rs
@@ -1,6 +1,13 @@
 #[path = "../src/proccore.rs"]
 mod proccore;
 
+fn get_result(result: Option<u32>) -> u32 {
+    match result {
+        Some(x) => x,
+        None    => panic!("Must be an instruction")
+    }
+}
+
 /* 
 Contained within are the tests for the translation of thumb instructions into ARM instructions
 
@@ -21,52 +28,52 @@ mod test {
     fn convert_soft_int() {
         let thumbinst: u16  = 0b1101111101000101;
         let arminst: u32    = 0b11101111000000000000000001000101;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_add_sp() {
         let thumbinst: u16  = 0b1011000001100110;
         let arminst: u32    = 0b11100000100011011101000001100110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1011000011100110;
         let arminst: u32    = 0b11100000010011011101000001100110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
     #[test]
 
     fn convert_push_pop_reg() {
         let thumbinst: u16  = 0b1011010000011111;
         let arminst: u32    = 0b11101001001011010000000000011111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1011010100011111;
         let arminst: u32    = 0b11101001001011010100000000011111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1011110001000100;
         let arminst: u32    = 0b11101000101111010000000001000100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1011110101000100;
         let arminst: u32    = 0b11101000101111011000000001000100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_ld_store_reg_offset() {
         let thumbinst: u16  = 0b0101000110010011;
         let arminst: u32    = 0b11100111100000100011000000000110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0101110111000010;
         let arminst: u32    = 0b11100111110100000010000000000111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
@@ -74,20 +81,20 @@ mod test {
         //arm format 8 = arm format 10
         let thumbinst: u16  = 0b0101001000011100;
         let arminst: u32    = 0b11100001100000110100000010010000;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0101101010101011;
         let arminst: u32    = 0b11100001100101010011000010110010;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0101011001111010;
         let arminst: u32    = 0b11100001100101110010000011010001;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0101111010100011;
         let arminst: u32    = 0b11100001100101000011000011110010;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         /* Need to test the other operations */
     }
 
@@ -95,127 +102,134 @@ mod test {
     fn convert_alu_operations() {
         let thumbinst: u16  = 0b0100000001100011;
         let arminst: u32    = 0b11100000001100110011000000000100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100000111000001;
         let arminst: u32    = 0b11100001101100010001000001110000;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100001001011101;
         let arminst: u32    = 0b11100010011100110101000000000000;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100001010110010;
         let arminst: u32    = 0b11100001010100100010000000000110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100001101111000;
         let arminst: u32    = 0b11100000000100000000000010010111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_hi_reg_operations() {
         let thumbinst: u16  = 0b0100010010101111;
         let arminst: u32    = 0b11100000100011111111000000000101;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100010101100100;
         let arminst: u32    = 0b11100001010101000100000000001100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100011011110111;
         let arminst: u32    = 0b11100001101011111111000000001110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0100011101011000;
         let arminst: u32    = 0b11100001001011111111111100011011;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_add_sub() {
         let thumbinst: u16  = 0b0001100100011000;
         let arminst: u32    = 0b11100000100100110000000000000100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b0001111110010110;
         let arminst: u32    = 0b11100010010100100110000000000110;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_uncond_branch() {
         let thumbinst: u16  = 0b1110011111111110;
         let arminst: u32    = 0b11101010000000000000001111111111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_pc_rel_load() {
         let thumbinst: u16  = 0b0100101111010011;
         let arminst: u32    = 0b11100101100111110011001101001100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_ld_str_halfword() {
         let thumbinst: u16  = 0b1000011100001110;
         let arminst: u32    = 0b11100001110000010110001110111000;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1000100010111100;
         let arminst: u32    = 0b11100001110101110100000010110100;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_sp_rel_ldstr() {
         let thumbinst: u16  = 0b1001010001111011;
         let arminst: u32    = 0b11100101100011010100000001111011;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_load_address() {
         let thumbinst: u16  = 0b1010001010001111;
         let arminst: u32    = 0b11100010100011110010001010001111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
         let thumbinst: u16  = 0b1010111000110101;
         let arminst: u32    = 0b11100010100011010110001000110101;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_load_mult_load_store() {
         let thumbinst: u16  = 0b1100000010001000;
         let arminst: u32    = 0b11101000101000000000000010001000;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 
     #[test]
     fn convert_cond_branch() {
         let thumbinst: u16  = 0b1101110000010111;
         let arminst: u32    = 0b11001010000000000000000000010111;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
+    }
+
+    #[test]
+    fn convert_long_branch_lnk() {
+        let thumbinst: u16  = 0b1111101010101010;
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(None, converted);
     }
 
     #[test]
     fn convert_mov_shft_reg() {
         let thumbinst: u16  = 0b0000111011101010;
         let arminst: u32    = 0b11100001101100000010110110100101;
-        let converted: u32  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
-        assert_eq!(arminst, converted);
+        let converted: Option<u32>  = proccore::translate_thumb(thumbinst, proccore::decode_thumb(thumbinst));
+        assert_eq!(arminst, get_result(converted));
     }
 }
