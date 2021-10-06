d202c365ae775e2048b3bfdc50a418c4d20ad6cf
RepeatedRoot <choeffe@gmail.com>
2021-10-06 20:12:40 +1030

    complete alu testing

--- tests/cpu_modules.rs
+++ tests/cpu_modules.rs
@@ -8,27 +8,164 @@ mod tests {
     #[test]
     fn alu_functions() {
         let mut core = arm7tdmi::Core::new();
+        core.setcond = true; //to set flags
 
+        /* AND */
+        core.aluop = 0;
+        core.abus = 0xFFFFFFFF;
+        core.barrelbus = 0xFFFFFFFF;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0xFFFFFFFF);
+        assert_eq!(u32::from(core.reg.cpsr), 0x80000000);
+        
+        /* EOR */
+        core.aluop = 1;
+        core.abus = 0x10101110;
+        core.barrelbus = 0x01010101;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0x11111011);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
+
+        /* SUB */
+        core.aluop = 2;
+        core.abus = 0xF0E0D0C0;
+        core.barrelbus = 0xFE000000;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0xF2E0D0C0);
+        assert_eq!(u32::from(core.reg.cpsr), 0xA0000000);
+
+        /* RSB */
+        core.aluop = 3;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0x0D1F2F40);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
+
+        /* ADD */
+        core.aluop = 4;
         core.abus = 0x01001123;
         core.barrelbus = 0x10321067;
-
-        core.aluop = 4;
-        core.setcond = true;
 
         core.alu();
 
         assert_eq!(core.alubus, 0x1132218A);
-        assert_eq!(u32::from(core.reg.cpsr), 0x0);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
 
+        /* ADC */
+        core.aluop = 5;
+        core.reg.cpsr.c = true;
+        core.abus = 0x12302113;
+        core.barrelbus = 0xEFAB1230;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0x01DB3344);
+        assert_eq!(u32::from(core.reg.cpsr), 0x20000000);
+
+        /* SBC */
+        core.aluop = 6;
+        core.abus = 0xF0E0D0C0;
+        core.barrelbus = 0xFE000000;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0xF2E0D0C0);
+        assert_eq!(u32::from(core.reg.cpsr), 0xA0000000);
+
+        /* RSC */
+        core.aluop = 7;
+        core.abus = 0xFDF00000;
+        core.barrelbus = 0x0FEEF001;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0x11FEF001);
+        assert_eq!(u32::from(core.reg.cpsr), 0x20000000);
+
+        core.reg.cpsr.c = false;
+        core.alubus = 0;
+
+        /* TST */
+        core.aluop = 8;
+        core.abus = 0xFFFFFFFF;
+        core.barrelbus = 0xFFFFFFFF;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0);
+        assert_eq!(u32::from(core.reg.cpsr), 0x80000000);
+
+        /* TEQ */
+        core.aluop = 9;
         core.abus = 3;
         core.barrelbus = 3;
-
-        core.aluop = 9;
-        core.alubus = 0;
 
         core.alu();
         
         assert_eq!(core.alubus, 0);
         assert_eq!(u32::from(core.reg.cpsr), 0x40000000);
+
+        /* CMP */
+        core.aluop = 0xA;
+        core.abus = 0xF0E0D0C0;
+        core.barrelbus = 0xFE000000;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0);
+        assert_eq!(u32::from(core.reg.cpsr), 0xA0000000);
+
+        /* CMN */
+        core.aluop = 0xB;
+        core.abus = 0x01001123;
+        core.barrelbus = 0x10321067;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
+
+        /* ORR */
+        core.aluop = 0xC;
+        core.abus = 0x10100101;
+        core.barrelbus = 0x13123132;
+
+        core.alu();
+        assert_eq!(core.alubus, 0x13123133);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
+
+        /* MOV */
+        core.aluop = 0xD;
+        
+        core.alu();
+
+        assert_eq!(core.alubus, 0x13123132);
+        assert_eq!(u32::from(core.reg.cpsr), 0);
+
+        /* BIC */
+        core.aluop = 0xE;
+        core.abus = 0xFFFFFFFF;
+        core.barrelbus = 0xFFFFFFFF;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0);
+        assert_eq!(u32::from(core.reg.cpsr), 0x40000000);
+
+        /* MVN */
+        core.aluop = 0xF;
+        core.barrelbus = 0x13123132;
+
+        core.alu();
+
+        assert_eq!(core.alubus, 0xECEDCECD);
+        assert_eq!(u32::from(core.reg.cpsr), 0x80000000);
     }
 }
